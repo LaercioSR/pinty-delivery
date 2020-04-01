c595f6a60117b7913ad8c2130e8975442af39691
Laercio Rios <laercio.rios13@hotmail.com>
2020-04-01 00:18:53 -0300

    configurações iniciais para o heroku

--- src/config/database.js
+++ src/config/database.js
@@ -6,19 +6,10 @@ module.exports = {
     database: 'pinty_delivery',
     host: '127.0.0.1',
     port: '3306',
-    dialect: 'mysql',
+    dialect: process.env.NODE_ENV == 'DEV' ? 'mysql' : 'postgres',
+    // dialect: 'mysql',
     define: {
         timestamps: true,
         underscored: true,
     },
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOSTNAME,
-    // port: process.env.DB_PORT,
-    // dialect: 'mysql',
-    // define: {
-    //     timestamps: true,
-    //     underscored: true,
-    // },
 }
