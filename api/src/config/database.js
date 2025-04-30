a049ab806270eb68016d80db6819dc7fa2b3abca
Laercio Rios <contact@laerciorios.com>
2025-04-30 10:12:36 -0300

    chore: update env example and remove not used lib

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,15 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    // dialect: 'mysql',
-    define: {
-        timestamps: true,
-        underscored: true,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOSTNAME,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+  // dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-}
+  },
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
