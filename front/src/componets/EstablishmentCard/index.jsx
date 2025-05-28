fa8754b6e7c3de88516c16856f918143263da867
cleiton cabral <cleitoncabral177@hotmail.com>
2025-05-27 21:52:57 -0300

    chore: establishment card renaming to english

---
+++ front/src/componets/EstablishmentCard/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
+
+import "./style.css";
+
+const EstablishmentCard = ({ establishment }) => {
+  return (
+    <div className="establishment-card">
+      <img
+        src={establishment.imagem_url}
+        alt="Imagem Estabelecimento"
+        className="profile-image"
+      />
+      <h3>{establishment.nome}</h3>
+
+      <p>{establishment.categoria}</p>
+    
+      <a
+        href={`https://api.whatsapp.com/send?phone=+55${establishment.whatsapp}&text=Olá, gostaria de fazer um pedido`}
+        type="button"
+        className="button-whatsapp"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span>
+          Fazer Pedido <FaWhatsapp color="#fff" />
+        </span>
+      </a>
+    </div>
+  );
+};
+
+export default EstablishmentCard;
--- front/src/pages/Home/index.jsx
+++ front/src/pages/Home/index.jsx
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from "react";
 import { Spinner } from "react-bootstrap";
 
 import NavbarPintyDelivery from "../../componets/NavbarPintyDelivery";
-import CardEstabelecimento from "../../componets/CardEstabelecimento";
+import EstablishmentCard from "../../componets/EstablishmentCard";
 
 import api from "../../services/api";
 import "./style.css";
 
 export default function Home() {
-  const [estabelecimentos, setEstabelecimentos] = useState([]);
+  const [establishments, setEstablishment] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     api.get("/estabelecimentos").then((response) => {
-      setEstabelecimentos(response.data);
+      setEstablishment(response.data);
       setLoading(false);
     });
   }, []);
@@ -29,12 +29,12 @@ export default function Home() {
           <h1>Estabelecimentos</h1>
           {loading ? (
             <Spinner animation="border" variant="danger" />
-          ) : estabelecimentos.length > 0 ? (
+          ) : establishments.length > 0 ? (
             <div className="grid">
-              {estabelecimentos.map((estabelecimento) => (
-                <CardEstabelecimento
-                  key={estabelecimento.id}
-                  estabelecimento={estabelecimento}
+              {establishments.map((establishment) => (
+                <EstablishmentCard
+                  key={establishment.id}
+                  establishment={establishment}
                 />
               ))}
             </div>
